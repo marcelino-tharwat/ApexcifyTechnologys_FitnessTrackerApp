db7391b8e84d714e344d6cc9c2b6c572c01201aa
marclenotharwaat <marclenotharwat@gmail.com>
2025-08-08 22:04:21 +0300

    setup main project

---
+++ lib/fitness_tracker_app.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class FitnessTrackerApp extends StatelessWidget {
+  const FitnessTrackerApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitness Tracker App',
+);
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:fitness_tracker_app/fitness_tracker_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fitness_tracker_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FitnessTrackerApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
